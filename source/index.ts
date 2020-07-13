b17344fbf90621e137bddacd914b6e76fb55b71c
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-07-13 18:03:37 -0400

    First building commit

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,2 @@
-export default
-function hello (): string {
-    return "It worked!";
-}
-
-console.log(hello());
+export * as PKIX_CommonTypes_2009 from "./PKIX-CommonTypes-2009";
+export * as PKIXCRMF_2009 from "./PKIXCRMF-2009";
