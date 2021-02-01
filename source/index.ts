e56bd9281edc93ec9ef9ba53bdd29ac6f15b0762
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-02-01 14:38:00 -0500

    v1.0.0
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- source/index.ts
+++
@@ -1,2 +0,0 @@
-export * as PKIX_CommonTypes_2009 from "./PKIX-CommonTypes-2009";
-export * as PKIXCRMF_2009 from "./PKIXCRMF-2009";
---
+++ source/modules/PKIXCRMF-2009/PrivateKeyAttributes.osa.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import { ATTRIBUTE } from "x500-ts/dist/node/modules/InformationFramework/ATTRIBUTE.oca";
+
+/* START_OF_SYMBOL_DEFINITION PrivateKeyAttributes */
+/**
+ * @summary PrivateKeyAttributes
+ * @description
+ *
+ * ### ASN.1 Definition:
+ *
+ * ```asn1
+ * PrivateKeyAttributes ATTRIBUTE ::= {...}
+ * ```
+ *
+ * @constant
+ * @type {ATTRIBUTE[]}
+ *
+ */
+export const PrivateKeyAttributes: ATTRIBUTE[] = [];
+/* END_OF_SYMBOL_DEFINITION PrivateKeyAttributes */
+
+/* eslint-enable */
